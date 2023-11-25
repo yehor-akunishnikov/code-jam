8485bfd4c6e9de9f642b000d8b2d082a4cf9ea05
Reonido <faiteforever@gmail.com>
2023-11-25 17:37:41 +0200

    updated cors options

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:4200',
-      'https://rzotr2.github.io/code-jam-ui'
+      'https://rzotr2.github.io'
     ],
     credentials: true
   });
