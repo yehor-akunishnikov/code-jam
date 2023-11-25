bf81b9609ec21cb167818e9a05dbc91948141b76
Reonido <faiteforever@gmail.com>
2023-11-25 19:47:29 +0200

    refactor

--- src/features/books/db/book.schema.ts
+++ src/features/books/db/book.schema.ts
@@ -1,5 +1,6 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+
+import {HydratedDocument} from 'mongoose';
 
 export type BookDocument = HydratedDocument<Book>;
 
--- src/features/books/services/books.service.spec.ts
+++ src/features/books/services/books.service.spec.ts
@@ -1,18 +1,19 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { BooksService } from './books.service';
+import {Test, TestingModule} from '@nestjs/testing';
+
+import {BooksService} from './books.service';
 
 describe('BooksService', () => {
-  let service: BooksService;
+    let service: BooksService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [BooksService],
+        }).compile();
 
-    service = module.get<BooksService>(BooksService);
-  });
+        service = module.get<BooksService>(BooksService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
