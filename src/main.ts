c444dad22fdf11d36cca5bcd4d6eb65aad475555
Reonido <faiteforever@gmail.com>
2023-11-25 17:12:36 +0200

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://rzotr2.github.io/code-jam-ui/'
+    ]
+  });
   await app.listen(process.env.PORT || 3000); // update this line
 }
 
