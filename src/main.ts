6788248f6543b448af8426556872fe4181126a7c
Reonido <faiteforever@gmail.com>
2023-11-25 17:33:20 +0200

    updated cors options again

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: [
       'http://localhost:4200',
       'https://rzotr2.github.io/code-jam-ui'
     ],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
   });
+
   await app.listen(process.env.PORT || 3000); // update this line
 }
 
