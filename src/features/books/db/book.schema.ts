e8e9ed29a9f5f3fda1cceeb83bd4f66d5e5e7efb
Reonido <faiteforever@gmail.com>
2023-11-25 21:07:29 +0200

    updated book schema

--- src/features/books/db/book.schema.ts
+++ src/features/books/db/book.schema.ts
@@ -1,13 +1,22 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 
-import {HydratedDocument} from 'mongoose';
+import {HydratedDocument, Types} from 'mongoose';
 
 export type BookDocument = HydratedDocument<Book>;
 
-@Schema()
+@Schema({
+    versionKey: false,
+})
 export class Book {
     @Prop({required: true}) name: string;
     @Prop({required: true}) url: string;
+    @Prop() id: Types.ObjectId;
 }
 
 export const BookSchema = SchemaFactory.createForClass(Book);
+
+BookSchema.method('toJSON', function () {
+    const { _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
