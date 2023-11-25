6a9efb315ac1101d7a0e42b485863334d15bc599
Reonido <faiteforever@gmail.com>
2023-11-25 20:38:54 +0200

    added post route

--- src/features/books/books.controller.ts
+++ src/features/books/books.controller.ts
@@ -1,7 +1,8 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 
 import {BooksService} from './services/books.service';
 import {Book} from './db/book.schema';
+import {CreateBookDto} from './db/book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -11,5 +12,9 @@ export class BooksController {
 
     @Get() public getAll(): Promise<Book[]> {
         return this.booksService.findAll();
+    }
+
+    @Post() public create(@Body() createBookDto: CreateBookDto): Promise<Book> {
+        return this.booksService.create(createBookDto);
     }
 }
